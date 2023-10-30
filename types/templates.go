d690b097f575367cafbdb7177c2e921a6700cb1a
RamiRond <ramirond@beaconcha.in>
2023-10-30 17:22:19 +0100

    (BIDS-2550) save contract updates to bigtable

--- types/templates.go
+++ types/templates.go
@@ -1593,6 +1593,13 @@ type Eth1AddressPageData struct {
 	Tabs               []Eth1AddressPageTabs
 }
 
+type IsContractAtRequest struct {
+	Address  string
+	Block    int64
+	TxIdx    int64
+	TraceIdx int64
+}
+
 type Eth1AddressPageTabs struct {
 	Id   string
 	Href string
